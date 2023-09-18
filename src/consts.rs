f534c1476e910ba14319048130e738a2bfef08b7
YanniAlshoufiHTL <114535252+YanniAlshoufiHTL@users.noreply.github.com>
2023-09-18 21:29:35 +0200

    Added separator for objects a player interacted with

--- src/consts.rs
+++ src/consts.rs
@@ -1,12 +1,13 @@
-pub const MAIN_SEPARATOR: &str = /*                 */ "{;}";
-pub const LOBBY_SEPARATOR: &str = /*                */ "{;;}";
-pub const MOODLE_OBJECT_SEPARATOR: &str = /*        */ "{;;;}";
-pub const PLAYER_SEPARATOR: &str = /*               */ "{;;;;}";
-pub const WORLD_SEPARATOR: &str = /*                */ "{;;;;;}";
-pub const COORDS_SEPARATOR: &str = /*               */ "{;;;;;;}";
-pub const LOBBY_PLAYER_MODELS_SEPARATOR: &str = /*  */ "{;;;;;;;}";
+pub const MAIN_SEPARATOR: &str = /*                         */ "{;}";
+pub const LOBBY_SEPARATOR: &str = /*                        */ "{;;}";
+pub const MOODLE_OBJECT_SEPARATOR: &str = /*                */ "{;;;}";
+pub const PLAYER_SEPARATOR: &str = /*                       */ "{;;;;}";
+pub const WORLD_SEPARATOR: &str = /*                        */ "{;;;;;}";
+pub const COORDS_SEPARATOR: &str = /*                       */ "{;;;;;;}";
+pub const LOBBY_PLAYER_MODELS_SEPARATOR: &str = /*          */ "{;;;;;;;}";
+pub const OBJECT_PLAYER_INTERACTED_WITH_SEPARATOR: &str = /**/ "{;;;;;;;;}";
 
-pub const SEPARATORS: [&str; 7] = [
+pub const SEPARATORS: [&str; 8] = [
     MAIN_SEPARATOR,
     LOBBY_SEPARATOR,
     MOODLE_OBJECT_SEPARATOR,
@@ -14,4 +15,5 @@ pub const SEPARATORS: [&str; 7] = [
     WORLD_SEPARATOR,
     COORDS_SEPARATOR,
     LOBBY_PLAYER_MODELS_SEPARATOR,
+    OBJECT_PLAYER_INTERACTED_WITH_SEPARATOR,
 ];
